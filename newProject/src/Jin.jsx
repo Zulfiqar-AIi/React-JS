7c6556f1b11ee78df3a3ccb03872033acdb8d816
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-01-11 02:02:51 +0500

    updates done

---
+++ newProject/src/Jin.jsx
@@ -0,0 +1,7 @@
+function Jin(){
+    return (
+    <h1>JIN BABO</h1>
+    )
+}
+
+export default Jin;
---
+++ notes.js
@@ -0,0 +1,5 @@
+console.log("Hello bhai jaan");
+
+// <>hello boss</>
+// The above code is wrapped inside a parent element called Fragment or any other valid HTML element, used in app.jsx file.
+// ------------------------------------------------------------
