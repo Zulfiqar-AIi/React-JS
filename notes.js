073d55fd2b1f7c13eb45c9211dc0f129d86c0f38
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-01-11 21:15:09 +0500

    notes updation as always

--- notes.js
+++ notes.js
@@ -3,3 +3,4 @@ console.log("Hello bhai jaan");
 // <>hello boss</>
 // The above code is wrapped inside a parent element called Fragment or any other valid HTML element, used in app.jsx file.
 // ------------------------------------------------------------
+// UI updation ko HOOKS control karta hai react mein.
