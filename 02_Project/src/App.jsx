4de8846964215821e0f9971337e0e81f126fe4ec
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-01-13 00:31:19 +0500

    new changes

--- 02_Project/src/App.jsx
+++ 02_Project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -12,19 +12,19 @@ function App() {
   const addValue = () => {
     //counter = counter + 1
     setCounter(prevCounter => prevCounter + 1)
-    setCounter(prevCounter => prevCounter + 1 )
-    setCounter(prevCounter => prevCounter + 1)
-    setCounter(prevCounter => prevCounter + 1)
+    console.log(counter + 1);
     
   }
 
   const removeValue = () => {
     setCounter(counter - 1)
+    console.log(counter - 1);
   }
+
   
   return (
     <>
-      <h1>Chai aur react</h1>
+      <h1 style={{ fontsize: '72px',color: 'red',}}>Reactuddin Sheikhopuro</h1>
       <h2>Counter value: {counter}</h2>
 
       <button
