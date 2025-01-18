56e43e7d24a7202c97ee735e25aef8c2e65b3b42
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-01-19 03:45:08 +0500

    project complete

--- 02_Project/src/App.jsx
+++ 02_Project/src/App.jsx
@@ -4,37 +4,34 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-  const [counter, setCounter]  = useState(15)
-
-  //let counter = 15
+  const [counter, setCounter] = useState(15)
 
   const addValue = () => {
-    //counter = counter + 1
-    setCounter(prevCounter => prevCounter + 1)
-    console.log(counter + 1);
-    
+    setCounter((prevCounter) => {
+      if (prevCounter < 30) {
+        return prevCounter + 1
+      }
+      return prevCounter
+    })
   }
 
   const removeValue = () => {
-    setCounter(counter - 1)
-    console.log(counter - 1);
+    setCounter((prevCounter) => {
+      if (prevCounter > 0) {
+        return prevCounter - 1
+      }
+      return prevCounter
+    })
   }
-
-  
   return (
     <>
-      <h1 style={{ fontsize: '72px',color: 'red',}}>Reactuddin Sheikhopuro</h1>
+      <h1 style={{ fontSize: '72px', color: 'red' }}>Reactuddin Sheikhopuro</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button
-      onClick={addValue}
-      >Add value {counter}</button> 
+      <button onClick={addValue}>Add value {counter}</button>
       <br />
-      <button
-      onClick={removeValue}
-      >remove value {counter}</button>
-      <p>footer: {counter}</p>
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>Footer: {counter}</p>
     </>
   )
 }
