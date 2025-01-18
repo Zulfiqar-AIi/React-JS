9e1ea32ed0d89dab4fb4fa4f929f45ed466b3ffb
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-01-18 21:02:52 +0500

    new changes

--- notes.js
+++ notes.js
@@ -4,3 +4,7 @@ console.log("Hello bhai jaan");
 // The above code is wrapped inside a parent element called Fragment or any other valid HTML element, used in app.jsx file.
 // ------------------------------------------------------------
 // UI updation ko HOOKS control karta hai react mein.
+// ------------------------------------------------------------
+// Hydration occurs when React attaches event listeners to the markup generated on the server. Sometimes it happens that html page loads and the buttons are'nt working properly. This is because the event listeners are not attached to the buttons. This is called hydration.
+// -------------------------------------------------------------
+// devui.io is used for taking components in tailwindcss
