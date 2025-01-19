fd598d44f42d2d567fc4268a0a9a1a6768f95cea
Zulfiqar-Ali <zulfiqarnizar60@gmail.com>
2025-01-20 00:29:46 +0500

    project completed

--- 03_Project/src/App.jsx
+++ 03_Project/src/App.jsx
@@ -7,8 +7,144 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='bg-yellow-400 text-black rounded-3xl'>MR. ZULFIQAR ALI</h1>
+    // <> is a short syntax for <React.Fragment>
+    <> 
+      <h1 className='bg-yellow-400 text-black rounded-3xl m-5'>MR. ZULFIQAR ALI</h1>
+{/* -------------------------------------------------------------------------------------------- */}
+      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 m-5">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512"/>
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Zulfiqar Ali
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Software Engineer, Pakistan 
+      </div>
+    </figcaption>
+  </div>
+</figure>
+{/* ---------------------------------------------------------------------------------- */}
+<div class="flex font-sans">
+  <div class="flex-none w-56 relative">
+    <img src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" class="absolute inset-0 w-full h-full object-cover rounded-lg" loading="lazy" />
+  </div>
+  <form class="flex-auto p-6 bg-blue-900 rounded-xl">
+    <div class="flex flex-wrap">
+      <h1 class="flex-auto font-medium text-slate-900">
+        Kids Jumpsuit
+      </h1>
+      <div class="w-full flex-none mt-2 order-1 text-3xl font-bold text-violet-600">
+        $39.00
+      </div>
+      <div class="text-sm font-medium text-slate-400">
+        In stock
+      </div>
+    </div>
+    <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+      <div class="space-x-2 flex text-sm font-bold">
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="xs" checked />
+          <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+            XS
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="s" />
+          <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+            S
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="m" />
+          <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+            M
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="l" />
+          <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+            L
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="xl" />
+          <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+            XL
+          </div>
+        </label>
+      </div>
+    </div>
+    <div class="flex space-x-4 mb-5 text-sm font-medium">
+      <div class="flex-auto flex space-x-4">
+        <button class="h-10 px-6 font-semibold rounded-full bg-violet-600 text-white" type="submit">
+          Buy now
+        </button>
+        <button class="h-10 px-6 font-semibold rounded-full border border-slate-200 text-slate-100" type="button" >
+          Add to bag
+        </button>
+      </div>
+
+      <button class="flex-none flex items-center justify-center w-9 h-9 rounded-full text-violet-600 bg-violet-50" type="button" aria-label="Like">
+        <svg width="20" height="20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </button>
+
+    </div>
+    <p class="text-sm text-purple-500">
+      Free shipping on all continental US orders.
+    </p>
+  </form>
+</div>
+{/* ------------------------------------------------------------------------------------------- */}
+
+<main className="py-6 px-4 sm:p-6 md:py-10 md:px-8">
+  <div className="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2 bg-slate-100 rounded-xl">
+    <div className="relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
+      <h1 className="mt-1 text-lg font-semibold text-white sm:text-slate-500 md:text-2xl dark:sm:text-black">Beach House in Collingwood</h1>
+      <p className="text-sm leading-4 font-medium text-white sm:text-slate-100 dark:sm:text-slate-900">Entire house</p>
+    </div>
+    <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+      <img src="https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy"/>
+      <img src="https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy"/>
+      <img src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className="hidden w-full h-52 object-cover rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy"/>
+    </div>
+    <dl className="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
+      <dt className="sr-only">Reviews</dt>
+      <dd className="text-indigo-600 flex items-center dark:text-indigo-400">
+        <svg width="24" height="24" fill="none" aria-hidden="true" className="mr-1 stroke-current dark:stroke-indigo-500">
+          <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <span>4.89 <span class="text-slate-500 font-normal">(128)</span></span>
+      </dd>
+      <dt className="sr-only">Location</dt>
+      <dd className="flex items-center text-black">
+        <svg width="2" height="2" aria-hidden="true" fill="currentColor" className="mx-3 text-slate-300">
+          <circle cx="1" cy="1" r="1" />
+        </svg>
+        <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-900" aria-hidden="true">
+          <path d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z" />
+          <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
+        </svg>
+        Collingwood, Ontario
+      </dd>
+    </dl>
+    <div className="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+      <button type="button" className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Check availability</button>
+    </div>
+    <p className="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-500">
+      This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.
+    </p>
+  </div>
+  </main>
     </>
   )
 }
